6ea4e19fbb0401b2a3ded9e9db2307ccdfbbc23c
Haakan T Johansson <f96hajo@chalmers.se>
2023-05-05 23:42:02 +0200

    Compile fix.

--- hbook/ext_file_writer.cc
+++ hbook/ext_file_writer.cc
@@ -1567,7 +1567,8 @@ void request_create_branch(void *msg,uint32_t *left)
 
       if (/* always: iter2->second._limit_min < 0 || */
 	  iter2->second._limit_max > item._var_array_len)
-	ERR_MSG("Controlling item (%s) for %s has limits [] outside array [].",
+	ERR_MSG("Controlling item (%s) for %s "
+		"has limits [%d,%d] outside array [%d,%d].",
 		item._var_ctrl_name, item._var_name,
 		iter2->second._limit_min, iter2->second._limit_max,
 		0, item._var_array_len);
