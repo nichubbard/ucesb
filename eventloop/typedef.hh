c7a2002c0a55577af008134c592c3959ddd76e8b
Haakan T Johansson <f96hajo@chalmers.se>
2023-07-14 15:33:02 +0200

    Ugly compile fixup.  (broken code)

--- eventloop/typedef.hh
+++ eventloop/typedef.hh
@@ -39,4 +39,50 @@ typedef unsigned short ushort;
 typedef unsigned char  uchar;
 #endif
 
+/* TODO: this really should be in its own file! */
+
+#include <inttypes.h>
+
+/* For really old compilers.
+ *
+ * Hmmm, this is actually really unhealthy, as arguments will be
+ * consumed wrongly.
+ */
+
+#ifndef PRId8
+#define PRId8   "d"
+#define PRIi8   "i"
+#define PRIo8   "o"
+#define PRIu8   "u"
+#define PRIx8   "x"
+#define PRIX8   "X"
+#endif
+
+#ifndef PRId16
+#define PRId16  "d"
+#define PRIi16  "i"
+#define PRIo16  "o"
+#define PRIu16  "u"
+#define PRIx16  "x"
+#define PRIX16  "X"
+#endif
+
+#ifndef PRId32
+#define PRId32  "d"
+#define PRIi32  "i"
+#define PRIo32  "o"
+#define PRIu32  "u"
+#define PRIx32  "x"
+#define PRIX32  "X"
+#endif
+
+#ifndef PRId64
+#define PRId64  "lld"
+#define PRIi64  "lli"
+#define PRIo64  "llo"
+#define PRIu64  "llu"
+#define PRIx64  "llx"
+#define PRIX64  "llX"
+#endif
+
 #endif//__TYPEDEF_HH__
