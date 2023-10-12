472b0c40c029becec3b5a678e55562feff013ce5
HTJ <f96hajo@chalmers.se>
2023-10-12 08:53:18 +0200

    11 bits is 2048, not 8192

--- tamex17sync/tamex17sync_user.cc
+++ tamex17sync/tamex17sync_user.cc
@@ -24,9 +24,9 @@
 #include "convert_picture.hh"
 #include "user.hh"
 
-#define COARSE_RANGE   0x2000
-#define COARSE_SHIFT   4
-#define COARSE_BINS    (COARSE_RANGE >> COARSE_SHIFT)
+#define COARSE_RANGE   0x800                          /* 2048 (11 bits) */
+#define COARSE_SHIFT   2
+#define COARSE_BINS    (COARSE_RANGE >> COARSE_SHIFT) /* 512 */
 
 uint32_t corr[8][16][8][16][COARSE_BINS];
 
