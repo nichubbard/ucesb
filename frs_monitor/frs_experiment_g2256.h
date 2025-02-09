e7202235dbb80482ddff585da4c46ea2df27b8b2
Nic Hubbard <n.hubbard@gsi.de>
2025-02-09 14:06:19 +0100

    Add G-22-00056 Mapping

---
+++ frs_monitor/frs_experiment_g2256.h
@@ -0,0 +1,20 @@
+#define EXPERIMENT_NAME "G-22-00056"
+
+std::map<int, std::string> names =
+{
+  { 0x100 , "FRS" },
+  { 0x200 , "Trav. MUSIC" },
+  { 0x300 , "MR-TOF" },
+};
+
+std::vector<int> expected = { 0x100, 0x200, 0x300 };
+
+// 16 FATIMA scalers and 64 FRS scalers
+static constexpr size_t SCALER_FRS_FRS_COUNT = 32;
+static constexpr size_t SCALER_FRS_MAIN_COUNT = 32;
+static constexpr size_t SCALER_COUNT = SCALER_FRS_FRS_COUNT + SCALER_FRS_MAIN_COUNT;
+
+static constexpr int FRS_TPAT_PULSER = (1 << 1);
+static constexpr size_t SCALER_START_EXTR = 8;
+static constexpr size_t SCALER_STOP_EXTR = 9;
+
