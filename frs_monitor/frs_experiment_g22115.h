9387f3a481a59e6bba735ffc7142ae0a7523ae9f
Nic Hubbard <n.hubbard@gsi.de>
2025-02-27 16:50:39 +0100

    Export WRID

--- frs_monitor/frs_experiment_g22115.h
+++ frs_monitor/frs_experiment_g22115.h
@@ -8,6 +8,14 @@ std::map<int, std::string> names =
   { 0x400 , "DESPEC-DEGAS" },
   { 0x500 , "DESPEC-bPlas" },
   { 0x700 , "DESPEC-AIDA" },
+  { 0xa00 , "EXPERT #1" },
+  { 0x1100 , "EXPERT #2" },
+  { 0x1300 , "EXPERT #3" },
+  { 0x1400 , "EXPERT #4" },
+  { 0x1500 , "EXPERT #5" },
+  { 0x1600 , "EXPERT #6" },
+  { 0x1700 , "EXPERT #7" },
+  { 0x1900 , "EXPERT #8" },
 };
 
 std::vector<int> expected = { 0x100, 0x200, 0x400, 0x500, 0x700 };
